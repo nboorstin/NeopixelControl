d511e4beebbe13d4af10a61face7bb5d2b5ec47a
Noah Boorstin <nboorstin@hmc.edu>
2020-07-15 18:34:35 -0400

    got some POST requests

--- first.py
+++ first.py
@@ -1,12 +1,19 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello world"
+def root():
+    return redirect(url_for('send_html', path='test.html'))
 
-@app.route("/<path>")
-def send_html(path):
-    return send_from_directory("html", path)
+@app.route("/<path>", methods=['GET'])
+def send_html(path, entered=None):
+    print(path)
+
+    print(entered)
+    return render_template(path, name = entered);
+
+@app.route("/response", methods=['POST'])
+def response():
+    return send_html(path='test.html', entered=request.form['entry'])
