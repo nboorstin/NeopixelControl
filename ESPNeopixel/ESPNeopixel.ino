cbd889b39843dae34362bed9cfa9ea7da95e21ca
Noah Boorstin <nboorstin@hmc.edu>
2020-07-18 14:44:49 -0400

    start messing around with sockets

--- ESPNeopixel/ESPNeopixel.ino
+++ ESPNeopixel/ESPNeopixel.ino
@@ -1,21 +1,26 @@
+// So apparently ESP8266-01 are a lot more obnoxious to deal with than I thought
+// http://www.whatimade.today/esp8266-easiest-way-to-program-so-far/
+// https://www.esp8266.com/viewtopic.php?f=6&t=9958
+
+
 #include <FastLED.h>
 
 #define LED_PIN D2
-#define NUM_LEDS 1
+#define NUM_LEDS 2
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  leds[0] = CRGB(255, 0, 0);
-  FastLED.show();
 }
 
 int i=0;
 
 void loop() {
-  Serial.print(".");  
+  //Serial.print(".");  
   leds[0] = CRGB(i = ++i % 255, 0, 0);
+  leds[1] = CRGB(i = ++i % 255, 0, 0);
+  delay(10);
   FastLED.show();
 }
--- first.py
+++ first.py
@@ -1,12 +1,30 @@
 from flask import Flask, send_from_directory, redirect, url_for, request, render_template, make_response, json
-from threading import Timer
+from flask_socketio import SocketIO, send, emit
+from threading import Timer, Lock
 
 app = Flask(__name__)
 
 import logging
 log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+#log.setLevel(logging.ERROR)
 
+socketio = SocketIO(app, heartbeat_interval=4, heartbeat_timeout=10)
+if __name__ == '__main__':
+    socketio.run(app)
+
+@socketio.on('my event')
+def handle_message(message):
+    print('recieved message: ' + str(message))
+    send('test', broadcast=True)
+
+@socketio.on('connect')
+def test_connect():
+    print("connected!")
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 @app.route("/")
 def root():
@@ -20,6 +38,7 @@ def send_static(path):
 def send_html(path, entered=None):
     return render_template(path, name = entered);
 
+
 data = {}
 
 with open('data.json') as f:
@@ -31,18 +50,23 @@ def writeFunc():
     print("done!")
 
 writeToFile = Timer(5, writeFunc)
+lock = Lock()
 
 @app.route("/response", methods=['POST'])
 def response():
     global writeToFile
+    lock.acquire()
     writeToFile.cancel()
     writeToFile = Timer(5, writeFunc)
+    writeToFile.start()
+    lock.release()
 
     data.update(request.json)
+    socketio.emit('data', data, broadcast=True)
+
 
     for i in data:
         print(i+":", data[i])
 
-    writeToFile.start()
 
     return make_response("test")
