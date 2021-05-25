b4b926be3707707d5248ce8751a407c2dffbbb33
Noah Boorstin <nboorstin@hmc.edu>
2021-05-25 11:30:30 -0700

    sockets!

--- server.rb
+++ server.rb
@@ -1,11 +1,28 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require 'sinatra-websocket'
 
 set :bind, '0.0.0.0'
 set :public_folder, __dir__ + '/static'
+set :sockets, []
 
 get '/' do
-  redirect to('/tabs.html')
+  if !request.websocket?
+    redirect to('/tabs.html')
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("Hello World!")
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        puts msg
+        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+      end
+      ws.onclose do
+        warn("websocket closed")
+        settings.sockets.delete(ws)
+      end
+    end
+  end
 end
-
-
