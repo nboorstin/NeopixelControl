aa2ea52028c43cbdd0196477628d125c9b95e00c
Noah Boorstin <nboorstin@hmc.edu>
2021-05-31 12:59:40 -0700

    small server update

--- server.rb
+++ server.rb
@@ -7,12 +7,34 @@ set :bind, '0.0.0.0'
 set :public_folder, __dir__ + '/static'
 set :sockets, []
 
+instances = ["test"]
+
 get '/' do
   redirect to('/test')
 end
 
-thread = nil
-instances = ["test"]
+$sites = []
+$esps = []
+$thread = nil
+def message(msg, ws, path)
+  puts msg
+  unless $thread.nil? || !$thread.status
+    Thread.kill($thread)
+  end
+  $thread = Thread.new(msg) {|msg|
+    sleep 5
+    puts "thread done"
+    hash = JSON.parse File.read "static/#{path}.json"
+    hash = hash.merge JSON.parse msg
+    File.open("static/#{path}.json","w") do |f|
+      f.write hash.to_json
+    end
+  }
+  puts settings.sockets
+  EM.next_tick { $esps.select{|s| s != ws}.each{|s| s.send(msg) } }
+  EM.next_tick { $sites.select{|s| s != ws}.each{|s| s.send(msg) } }
+end
+
 instances.each do |path|
   get "/#{path}" do
     instance = request.path_info[1..-1]
@@ -22,30 +44,30 @@ instances.each do |path|
   get "/#{path}/site" do
     request.websocket do |ws|
       ws.onopen do
-        ws.send("Hello World!")
-        settings.sockets << ws
+        #ws.send("Hello World!")
+        $sites.append(ws)
       end
       ws.onmessage do |msg|
-        puts msg
-        unless thread.nil? || !thread.status
-          Thread.kill(thread)
-        end
-        thread = Thread.new(msg) {|msg|
-          sleep 5
-          #puts "thread done"
-          hash = JSON.parse File.read "static/#{path}.json"
-          hash = hash.merge JSON.parse msg
-          #puts hash
-          File.open("static/#{path}.json","w") do |f|
-            f.write hash.to_json
-          end
-          #puts msg
-        }
-        #EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        message msg, ws, path
       end
       ws.onclose do
         warn("websocket closed")
-        settings.sockets.delete(ws)
+        $sites.delete(ws)
+      end
+    end
+  end
+  get "/#{path}/esp" do
+    request.websocket do |ws|
+      ws.onopen do
+        #ws.send("Hello World!")
+        $esps.append(ws)
+      end
+      ws.onmessage do |msg|
+        message msg, ws, path
+      end
+      ws.onclose do
+        warn("websocket closed")
+        $esps.delete(ws)
       end
     end
   end
