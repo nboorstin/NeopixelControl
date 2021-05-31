fd5d360fa1dd6cf4efe6ea12362a46c9e1695614
Noah Boorstin <nboorstin@hmc.edu>
2021-05-31 07:21:22 -0700

    small server updates

--- server.rb
+++ server.rb
@@ -7,6 +7,11 @@ set :bind, '0.0.0.0'
 set :public_folder, __dir__ + '/static'
 set :sockets, []
 
+get '/' do
+  redirect to('/test')
+end
+
+thread = nil
 instances = ["test"]
 instances.each do |path|
   get "/#{path}" do
@@ -22,14 +27,20 @@ instances.each do |path|
       end
       ws.onmessage do |msg|
         puts msg
-        hash = JSON.parse File.read "static/#{path}.json"
-        hash = hash.merge JSON.parse msg
-        #puts hash
-        File.open("static/#{path}.json","w") do |f|
-          f.write hash.to_json
+        unless thread.nil? || !thread.status
+          Thread.kill(thread)
         end
-        
-
+        thread = Thread.new(msg) {|msg|
+          sleep 5
+          #puts "thread done"
+          hash = JSON.parse File.read "static/#{path}.json"
+          hash = hash.merge JSON.parse msg
+          #puts hash
+          File.open("static/#{path}.json","w") do |f|
+            f.write hash.to_json
+          end
+          #puts msg
+        }
         #EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
       end
       ws.onclose do
